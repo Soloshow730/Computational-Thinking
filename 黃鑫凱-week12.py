bf61541cd9813a2f11835e2836a7d31dc47d9a7f
Soloshow730 <71249875+Soloshow730@users.noreply.github.com>
2020-11-30 15:01:41 +0800

    Add files via upload

---
+++ 黃鑫凱-week12.py
@@ -0,0 +1,16 @@
+favorite_list=["movie","dance","DJ","sing","live show"]
+print(favorite_list[4])
+
+
+season = "1995-1996"
+team = "Chicago Bulls"
+coach = "Phil Jackson"
+records = [56, 28]
+starting_lineup = ["Ron Harper", "Michael Jordan", "Scottie Pippen", "Dennis Rodman", "Luc Longley"]
+won_championship = True
+best_chicago_bulls = list((season, team, coach, records,
+starting_lineup, won_championship))
+print(best_chicago_bulls)
+print(type(best_chicago_bulls))
+# indexing 索引
+print(best_chicago_bulls[0])
